215fcbba94aff763afa329cc0e2627465dbda20a
valencprado <valenpschool@gmail.com>
2021-10-28 22:14:02 -0300

    Erro

---
+++ erro.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Minha Loja - Logon de usuário</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style type="text/css">
+        .btn.custom-btn {
+            background-color: lightcyan;
+        }
+
+        .navbar {
+            margin-bottom: 0;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    //nome dos meus arquivos//
+    include 'conexao.php';
+    include 'nav.php';
+    include 'cabecalho.html';
+
+    ?>
+
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-sm-4 col-sm-offset-4 text-center">
+
+                <h2 style="font-size: 180%;">Usuário ou senha inválidos!</h2>
+                <br />
+                <!--conferir o nome do arquivo form de login do usuário aqui-->
+                <a href="formlogin.php" class="btn btn-lg btn-block custom-btn" role="button">Tentar Novamente</a>
+
+                <button type="button" class="btn btn-lg btn-block btn-outline-danger">Ainda não sou cadastrado</button>
+
+            </div>
+        </div>
+    </div>
+
+    <?php include 'rodape.html' ?>
+
+</body>
+
+</html>
