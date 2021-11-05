f4adcfbb339932f29e5593bf6bd25e14ac93964e
valencprado <valenpschool@gmail.com>
2021-11-05 17:57:33 -0300

    adaptação à loja de discos

--- insdisco.php
+++ insdisco.php
@@ -1,16 +1,16 @@
 <?php
 
 include 'conexao.php';  // include com arquivo de conexao
-
+include 'resize-class.php';
 //variáveis que vão receber os dados do formulário que esta na pagina formProduto
 
 $cd_cat = $_POST['sltcat'];  // recebendo o valor do campo select, valor numérico
-$nome_livro = $_POST['txtlivro'];
+$nome_disco = $_POST['txtdisco'];
 $cd_banda = $_POST['sltbanda']; // recebendo o valor do campo select, valor numérico
 $preco = $_POST['txtpreco'];
 $qtde = $_POST['txtqtde'];
 $descricao = $_POST['txtdescricao'];
-$lanc = $_POST['sltlanc'];
+$novi = $_POST['sltnovi'];
 
 $remover1='.';  // criando variável e atribuindo o valor de ponto para ela
 $preco = str_replace($remover1, '', $preco); // removendo ponto de casa decimal,substituindo por vazio
@@ -34,9 +34,11 @@ $img_nome1 = md5(uniqid(time())).".".$extencao1[1];
 
 
 try {  // try para tentar inserir
-	
-	$inserir=$cn->query("INSERT INTO tbl_disco(, cd_categoria, nm_livro, cd_banda, vl_preco, qt_estoque, ds_resumo_disco, ds_capa, sg_lancamento) VALUES ('$isbn', '$cd_cat', '$nome_livro', '$cd_autor', '$nopag', '$preco', '$qtde', '$resumo', '$img_nome1', '$lanc')");
-	
+	$inserir=$cn->query("INSERT INTO tbl_disco(cd_categoria, nm_disco, cd_banda, vl_preco, qt_estoque, ds_resumo_disco, ds_capa, sg_lancamento) VALUES ($cd_cat, '$nome_disco', '$cd_banda', $preco, $qtde, '$descricao', '$img_nome1', '$novi')");
+	move_uploaded_file($recebe_foto1['tmp_name'], $destino.$img_nome1);             
+$resizeObj = new resize($destino.$img_nome1);
+$resizeObj -> resizeImage(900, 640, 'crop');
+$resizeObj -> saveImage($destino.$img_nome1, 100);
 	
 }catch(PDOException $e) {  // se houver algum erro explodir na tela a mensagem de erro
 	
